98a42ae0d8cb6b65ac69b4580e8efb0a7fa5f885
HugoFG02k <94490436+HugoFG02k@users.noreply.github.com>
2021-11-22 16:36:21 -0600

    Add files via upload

---
+++ proyecto final/trastornos.pl
@@ -0,0 +1,71 @@
+
+enfermedad(fobia,[
+    "deterioro social",
+    "llantos",
+    "rabietas",
+    "paralisis",
+    "persistencia de sintomas",
+    "sucesos traumaticos",
+    "evitar situaciones",
+])
+
+enfermedad(ansiedadG,[
+    "ansiedad",
+    "preocupacion", % Le quité el acento
+    "inquietud",
+    "facilidad para fatigarse",
+    "falta de concentracion", % Dificultad para fatigarse
+    "irritable",
+    "tension muscular", % Le quité el acento
+    "dificultad para dormir", % Dificultad para dormirse o para continuar durmiendo, o sueño inquieto e insatisfactorio
+    "dificultad para controlar la preocupacion"
+])
+
+enfermedad(depresionP,[
+    "depresion",
+    "irritable",
+    "poco apetito",
+    "sobrealimentacion",
+    "fatiga",
+    "Baja autoestima",
+    "falta de concentracion",
+    "falta de concentracion",
+    "persistencia",
+])
+
+enfermedad(depresionM,[
+    "depresion",
+    "perdida de peso",
+    "perdida de apetito",
+    "aumento de peso",
+    "aumento de apetito",
+    "fatiga",
+    "falta de concentracion",
+    "malestar significativo",
+])
+
+syntoma("depresion")
+syntoma("deterioro social")
+syntoma("llantos")
+syntoma("rabietas")
+syntoma("paralisis")
+syntoma("persistencia de sintomas")
+syntoma("sucesos traumaticos")
+syntoma("evitar situaciones")
+syntoma("ansiedad")
+syntoma("preocupacion")
+syntoma("inquietud")
+syntoma("facilidad para fatigarse")
+syntoma("falta de concentracion")
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
+syntoma()
