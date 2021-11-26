cd855c06eb27a4fa0d980ec82c9998bbe8d4911f
Alejandro Padilla Flores <padillafloresalejandro99@gmail.com>
2021-11-26 15:36:12 -0600

    Agrega nuevas reglas

--- proyecto final/main.pl
+++ proyecto final/main.pl
@@ -4,38 +4,33 @@ inicio :-
     enfermedad(X), write("Su enfermedad es "), write(X);
     not(enfermedad(_)), write("Usted no cuenta con ninguna enfermedad"). 
 
-enfermedad(A) :-
-    enfermedades(Enfermedades),
-    Known0 = [],
-    phrase(any_enfermedad(Enfermedades, A), [Known0], _).
 
-any_enfermedad([Enfermedad|Enfermedades], A) -->
-    any_enfermedad_(Enfermedad, Enfermedades, A).
+is_true(Q) :-
+        format("~w\n", [Q]),
+        read(yes).
 
-any_enfermedad_(enfermedad(A0, []), Enfermedades, A) -->
-    (   { A0 = A }
-    ;   any_enfermedad(Enfermedades, A)
-    ).
-any_enfermedad_(enfermedad(A0, [C|Cs]), Enfermedades, A) -->
-    state0_state(Known0, Known),
-    { condition_truth(C, T, Known0, Known) },
-    next_enfermedad(T, enfermedad(A0,Cs), Enfermedades, A).
 
-next_enfermedad(yes, Enfermedad, Enfermedades, A)  --> any_enfermedad([Enfermedad|Enfermedades], A).
-next_enfermedad(no, _, Enfermedades, A)        --> any_enfermedad(Enfermedades, A).
+/* crear nuevo conocimiento */
+ crate_a_rule:-
+    Cond=[is_true("Hola"),is_true("mundo")],
+    list_to_tuple(Cond,Body),
+    assert(enfermedad(duelepansa):-Body),
+    save.
 
-state0_state(S0, S), [S] --> [S0].
+list_to_tuple([],_) :- 
+    ValidDomain='[x|xs]',
+    Culprit='[]',
+    Formal=domain_error(ValidDomain, Culprit),
+    Context=context('list_to_tuple','Cannot create empty tuple!'),
+    throw(error(Formal,Context)).
 
-condition_truth(is_true(Q), Answer, Known0, Known) :-
-    if_(known_(Q,Answer,Known0),
-        Known0 = Known,
-        ( format("~w\n", [Q]),
-            read(Answer),
-            Known = [known(Q,Answer)|Known0])).
+list_to_tuple([X],X).
 
-known_(What, Answer, Known, Truth) :-
-    if_(memberd_t(known(What,yes), Known),
-        ( Answer = si, Truth = true ),
-        if_(memberd_t(known(What,no), Known),
-            ( Answer = no, Truth = true),
-            Truth = false)).
+list_to_tuple([H|T],(H,Rest_Tuple)) :-
+    list_to_tuple(T,Rest_Tuple).
+
+/*  guardar los nuevos conocimientos */
+save:-
+    tell('trastornos.pl'),
+    listing(enfermedad),
+    told().
