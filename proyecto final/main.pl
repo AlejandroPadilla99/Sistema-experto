848bffe4743603cc1c089aae8c269b8feee0f341
Alejandro Padilla Flores <padillafloresalejandro99@gmail.com>
2021-11-28 09:42:24 -0600

    add_question

--- proyecto final/main.pl
+++ proyecto final/main.pl
@@ -1,20 +1,35 @@
 inicio :-
     consult("trastornos"),
-    consult("reif"),
-    enfermedad(X), write("Su enfermedad es "), write(X);
-    not(enfermedad(_)), write("Usted no cuenta con ninguna enfermedad"). 
+    enfermedad(X), write("Su enfermedad es "), write(X); not(enfermedad(_)), write("Usted no cuenta con ninguna enfermedad"). 
 
 
 is_true(Q) :-
         format("~w\n", [Q]),
         read(yes).
+/* Ingresar una nueva regla */
+ make_rule:-
+    write("Ingresa en nombre de la enfermedad"),read(S),
+    add_questions([],Lista),
+    write(Lista).
+
+add_questions(L,Lista):-
+    write("Ingrese una pregunta entre comilla porvafor GRACIAS"),read(Q),
+    Question = is_true(Q),
+    Temp = [Question|L],
+    write("Ingrese 1 para ingresar otra pregunta"),read(Ans),
+    (Ans = 1 ->
+        add_questions(Temp,Lista)
+        ;
+        Lista = Temp
+    ).
+
+    
 
 
 /* crear nuevo conocimiento */
- crate_a_rule:-
-    Cond=[is_true("Hola"),is_true("mundo")],
+ crate_a_rule(Name,Cond):- 
     list_to_tuple(Cond,Body),
-    assert(enfermedad(duelepansa):-Body),
+    assert(enfermedad(Name):-Body),
     save.
 
 list_to_tuple([],_) :- 
