69c7051915bd30fec9377e32d2c24538a602ed00
Alejandro Padilla Flores <padillafloresalejandro99@gmail.com>
2021-12-03 12:36:18 -0600

    Grafico

--- proyecto final/main.pl
+++ proyecto final/main.pl
@@ -1,11 +1,72 @@
-inicio :-
-    consult("trastornos"),
-    enfermedad(X), write("Su enfermedad es "), write(X); not(enfermedad(_)), write("Usted no cuenta con ninguna enfermedad"). 
+:- use_module(library(pce)).
+:- use_module(library(pce_style_item)).
+
+%   menu principal
+inicio:-
+    consult('trastornos'),
+	new(Menu, dialog('Sistema experto', size(1000,800))),
+	new(L,label(nombre,'Sistema de diagnostico')),
+	new(A,label(nombre,'Alejandro el mero mero')),
+	new(@texto,label(nombre,'responde un breve cuestionario para su diagnostico')),
+	new(@respl,label(nombre,'')),
+	new(Salir,button('SALIR',and(message(Menu, destroy),message(Menu,free)))),
+	new(@boton,button('realizar test',message(@prolog,consulta))),
+
+
+	send(Menu,append(L)),new(@btncarrera,button('Diagnostico?')),
+	send(Menu,display,L,point(125,20)),
+	send(Menu,display,A,point(80,360)),
+	send(Menu,display,@boton,point(100,150)),
+	send(Menu,display,@texto,point(20,100)),
+	send(Menu,display,Salir,point(20,400)),
+	send(Menu,display,@respl,point(20,130)),
+
+	send(Menu,open_centered).
+%   submenu
+%Menu de consulta
+consulta:-
+    enfermedad(X),
+    new(Res,dialog('Sistema experto')),
+    new(Info,dialog('Usted sufre de')),
+    new(Ans,label(ans,X)),
+    send(Res,append(Info)),
+    send(Res,append(Ans)),
+    send(Res,open_centered).
+
+
+%Ventana que muestra las correspondientes preguntas 
+:-dynamic si/1, no/1.
+preguntar(Pregunta):-
+    new(Di,dialog('Diagnostico')),
+    new(L2,label(texto,'Respode la siguiente pregunta')),
+    new(La,label(pre,Pregunta)),
+    new(B1,button(si,and(message(Di,return,si)))),
+    new(B2,button(no,and(message(Di,return,no)))),
+
+    send(Di,append(L2)),
+    send(Di,append(La)),
+    send(Di,append(B1)),
+    send(Di,append(B2)),
+    send(Di,open_centered),
+    get(Di,confirm,Answer),
+    write(Answer),
+    send(Di,destroy),
+    ( Answer = si ->
+        assert(si(Pregunta))
+        ;
+	    assert(no(Pregunta)),
+        fail
+    ).
 
 
 is_true(Q) :-
-        format("~w\n", [Q]),
-        read(yes).
+    (si(Q)->true;(no(Q)->fail; preguntar(Q))).
+
+
+limpiar :- retract(si(_)),fail.
+limpiar :- retract(no(_)),fail.
+limpiar.
+
 /* Ingresar una nueva regla */
  make_rule:-
     write("Ingresa en nombre de la enfermedad"),read(S),
@@ -23,10 +84,8 @@ add_questions(L,Lista):-
         Lista = Temp
     ).
 
-    
 
-
-/* crear nuevo conocimiento */
+%crea un nueva regla en la vase de dados y la inserta
  crate_a_rule(Name,Cond):- 
     list_to_tuple(Cond,Body),
     assert(enfermedad(Name):-Body),
@@ -44,7 +103,7 @@ list_to_tuple([X],X).
 list_to_tuple([H|T],(H,Rest_Tuple)) :-
     list_to_tuple(T,Rest_Tuple).
 
-/*  guardar los nuevos conocimientos */
+%Guarada los cambios en la base de datos
 save:-
     tell('trastornos.pl'),
     listing(enfermedad),
